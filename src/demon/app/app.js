89728877f2f369c0a0cce56a8034437708d4dd6c
artificial-life <proteus.protocol@gmail.com>
2016-03-29 19:37:22 +0600

    shorted

--- src/demon/app/app.js
+++ src/demon/app/app.js
@@ -11,17 +11,14 @@ let password = '1';
 
 dd.login(login, password).then(() => {
 	console.log('connection complete');
+
 	let ws = dd.getWorkstation(workstation_type);
 
-	let baund_rate = ws.baund_rate;
-	let data_bits = ws.data_bits;
-	let parity = ws.parity;
-	let stop_bits = ws.stop_bits;
 	let port = new SerialPort(ws.port, {
-		baudRate: baund_rate,
-		dataBits: data_bits,
-		parity: parity,
-		stopBits: stop_bits
+		baudRate: ws.baund_rate,
+		dataBits: ws.data_bits,
+		parity: ws.parity,
+		stopBits: ws.stop_bits
 	});
 
 	port.on('data', function (data) {
