1e1dd5aa59e0c80212e677f44305b0b0f84cc999
artificial-life <proteus.protocol@gmail.com>
2016-03-30 17:19:50 +0600

    event owner changed

--- src/demon/app/classes/workstations/DigitalDisplayWorkstation.js
+++ src/demon/app/classes/workstations/DigitalDisplayWorkstation.js
@@ -8,7 +8,10 @@ class DigitalDisplayWorkstation extends Base {
 		this.request_shared = ['timezone', 'office', 'services', 'organization-chain'];
 	}
 	middleware() {
-		return this.subscribe('digital-display.command', (event) => {
+		return this.subscribe({
+			name: 'digital-display.command',
+			owner_id: this.getId()
+		}, (event) => {
 			console.log('<DD> new command:', event);
 			let event_data = event.data;
 			this.emit('command', new Buffer(event_data.data, 'hex'));
